c62c51a688ed8785ec8c8f6b1354ff9a0484869e
meisken <meisken999@gmail.com>
2020-11-11 07:38:28 +0800

    disable all scrolling animation on moblie

--- src/components/on_scrolling_animation/ScrollingAnimationTrigger.js
+++ src/components/on_scrolling_animation/ScrollingAnimationTrigger.js
@@ -52,8 +52,8 @@ export default function ScrollingAnimationTrigger({children,beforeActivateClassN
 
 
     return (
-        <span ref={el} className={beforeActivateClassName + (activation ? " activated" : "")}>
+        <div ref={el} className={beforeActivateClassName + (activation ? " activated" : "")}>
             {children}
-        </span>
+        </div>
     )
 }
