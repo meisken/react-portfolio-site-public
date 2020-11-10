7ca224d355ca0bfc51966fb3511b782178f940c2
meisken <meisken999@gmail.com>
2020-11-11 07:06:09 +0800

    added scrolling animation to about me

--- src/components/about/about_me/AboutMe.js
+++ src/components/about/about_me/AboutMe.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import Title from '../UnderlineTitle'
 import InvertedButton from '../../global/InvertedButton'
+import ScrollingAnimationTrigger from '../../on_scrolling_animation/ScrollingAnimationTrigger'
 export default function AboutMe() {
     return (
         <div className="about-me">
             
             <Title key="1">About me</Title>
 
-           
-            <div className="hero">
-                
-                <h3 className="SegoeUI-font semibold">I'm Ken Tang<br/>I'm Web Developer<br/>I can build you a wonderful website</h3>
-                <p className="SegoeUI-font light">I love using technology and building beautiful things.</p>
-                <InvertedButton>download my resume</InvertedButton>
-            </div>
+            <ScrollingAnimationTrigger beforeActivateClassName="about-hero-scrolling-animation">
+                <div className="hero">
+                    
+                    <h3 className="SegoeUI-font semibold">I'm Ken Tang<br/>I'm Web Developer<br/>I can build you a wonderful website</h3>
+                    <p className="SegoeUI-font light">I love using technology and building beautiful things.</p>
+                    <InvertedButton>download my resume</InvertedButton>
+                </div>
+            </ScrollingAnimationTrigger>
+
 
         </div>
     )
