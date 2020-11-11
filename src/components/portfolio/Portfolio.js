2f7b2f464f0893bea8cd2bacb48ecac1a2d37cba
meisken <meisken999@gmail.com>
2020-11-12 06:14:32 +0800

    Supplemented portfolio video

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -10,7 +10,7 @@ export default function Portfolio() {
     const webs = [
         {num:"01",heading:"Audio Streaming Website",segment:(<span>Enjoy amazing music here and relax with the music live demo: <a href="https://meiskena-audio.web.app/">audio.com</a></span>),videoPath:require('../../video/audio_website.mp4'),poster:require('../../img/audio.png')},
         {num:"02",heading:"Online Calendar Web app",segment:(<span>Manage your time. Reduce unnecessary time spending live demo: <a href="https://calendar-957dd.web.app/">calendar.com</a></span>),videoPath:require('../../video/calendar_website.mp4'),poster:require('../../img/schedule.png')},
-        {num:"03",heading:"Personal Portfolio Website",segment:"Here is the showcase which placing my works. It shows apart of my ability.",videoPath:require('../../video/audio_website.mp4'),poster:require('../../img/audio.png')}
+        {num:"03",heading:"Personal Portfolio Website",segment:"Here is the showcase which placing my works. It shows apart of my ability.",videoPath:require('../../video/portfolio.mp4'),poster:require('../../img/portfolioSite.PNG')}
     ];
     return (
         <div className="portfolio" id="portfolio-page">
